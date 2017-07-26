847047fbd70d500c92444f9b09b212daf26a939a
hjbj <huangjing_93@126.com>
2017-07-27 00:13:49 +0800

    add Aggregate Example

---
+++ src/main/scala/AggregateExample.scala
@@ -0,0 +1,22 @@
+/**
+  * Created by huangjing on 2017/7/27.
+  */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object AggregateExample {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local").setAppName("MapExample")
+    val sc = new SparkContext(sparkConf)
+
+    val inRdd = sc.parallelize(List(1.0,2,3,4),2)
+    val sumCount = inRdd.aggregate((0.0,0))(
+                        (acc,value) => (acc._1+value,acc._2+1),
+                        (acc1,acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+
+    val ave = sumCount._1 / sumCount._2
+
+    println(ave)
+  }
+}
