12d08bc19254c3b519b20c13c0b1212c583a6e97
hjbj <huangjing_93@126.com>
2017-08-31 23:04:15 +0800

    modify spark appname

--- src/main/scala/AggregateExample.scala
+++ src/main/scala/AggregateExample.scala
@@ -7,7 +7,7 @@ import org.apache.spark.SparkConf
 
 object AggregateExample {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local").setAppName("MapExample")
+    val sparkConf = new SparkConf().setMaster("local").setAppName("AggregateExample")
     val sc = new SparkContext(sparkConf)
 
     val inRdd = sc.parallelize(List(1.0,2,3,4),2)
